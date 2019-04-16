9bf42e355338fed55e2363b38a2cb9ae96c93700
Raffael C. Rossi <raffixrcr@hotmail.com>
2019-04-15 23:16:08 -0300

    Criada e ajustada tela de login.

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -5,34 +5,41 @@ import './../styles/NavigationFormStyle.css';
 import './../styles/InputTextStyle.css';
 import './../styles/ActionsButtonsStyle.css';
 import './../styles/Formulario.css';
+import './../styles/LoginStyle.css';
 import FormTitleComponent from './../components/FormTitleComponent';
 
 class Login extends Component {
   sendLogin() {
+    console.log('sendlogin');
     document.getElementById('linkInicial').click();
+  }
+
+  recoveryPassword() {
+    console.log('recovery password');
   }
 
   render() {
     return (
       <div className="App">
-        <FormTitleComponent title={'Login'} />
-        <div className="InputText">
-          <label className="LabelInputText">
-            <span>Usuário</span>
-            <input type="text" name="login" id="login" placeholder="Usuário" />
-          </label>                
+        <div className="ContainerLogin">
+          <div className="InputText">
+            <label className="LabelInputText">
+              <span>Usuário</span>
+              <input type="text" name="login" id="login" placeholder="Usuário" />
+            </label>                
+          </div>
+          <div className="InputText">
+            <label className="LabelInputText">
+              <span>Senha</span>
+              <input type="password" name="senha" id="senha" placeholder="Senha" />
+            </label>                
+          </div>
+          <div>
+            <button className="ActionsButtonsPrimary" onClick={this.sendLogin}>Entrar</button>
+            <a className="ActionsButtonsLink" onClick={this.recoveryPassword}>Recuperar senha</a>
+          </div>
+          <Link id="linkInicial" to="/Inicial"></Link>
         </div>
-        <div className="InputText">
-          <label className="LabelInputText">
-            <span>Senha</span>
-            <input type="password" name="senha" id="senha" placeholder="Senha" />
-          </label>                
-        </div>
-        <div>
-          <button className="ActionsButtonsPrimary" onClick="sendLogin">Entrar</button>                
-          <a className="ActionsButtonsLink" onClick="recoveryPassword">Recuperar senha</a>
-        </div>
-        <Link id="linkInicial" to="/Inicial"></Link>
       </div>
     );
   }
